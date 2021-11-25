322c2a91d37f2a1a82094d63c2751772baa753fe
Misode <Misoloo64@gmail.com>
2021-11-25 03:05:56 +0100

    Create modules.json with metadata

--- gm4.py
+++ gm4.py
@@ -1,25 +1,48 @@
 from beet.toolchain.helpers import subproject
 from beet import Context
+import json
 
 BASE = "base"
 OUTPUT = "out"
+RELEASE = "release"
 
 def build_modules(ctx: Context):
-	modules = [p.name for p in ctx.directory.glob("gm4_*")]
+	modules = [{"id": p.name} for p in ctx.directory.glob("gm4_*")]
 	print(f"[GM4] Found {len(modules)} modules")
 
+	try:
+		with open(f"{RELEASE}/modules.json", "r") as f:
+			released_modules = json.load(f)
+	except:
+		released_modules = [{"id": "gm4_bat_grenades", "patch": 3}]
+
 	for module in modules:
+		id = module["id"]
+		try:
+			with open(f"{id}/pack.mcmeta", "r") as f:
+				meta = json.load(f)
+		except:
+			module["id"] = None
+			continue
+
 		ctx.require(subproject({
-			"id": module,
+			"id": id,
 			"data_pack": {
-				"name": module,
-				"load": [BASE, module],
+				"name": id,
+				"load": [BASE, id],
 				"zipped": True,
 			},
 			"output": OUTPUT
 		}))
-		print(module)
 
-	with open(f"{OUTPUT}/modules.txt", "w") as f:
-		f.write("\n".join(modules))
+		patch = next((m["patch"] for m in released_modules if m["id"] == id), 0)
+		module["name"] = meta.get("module_name", id)
+		module["description"] = meta.get("site_description", "")
+		module["categories"] = meta.get("site_categories", [])
+		module["hidden"] = meta.get("hidden", False)
+		module["patch"] = patch + 1
+		print(id)
+
+	with open(f"{OUTPUT}/modules.json", "w") as f:
+		json.dump([m for m in modules if m.get("id") is not None], f, indent=2)
 		f.write('\n')
