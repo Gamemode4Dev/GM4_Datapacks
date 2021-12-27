aec432f79720572ed2d79c72237b6d44244d25e3
Misode <Misoloo64@gmail.com>
2021-12-27 19:45:28 +0100

    Add outdated module checker commands

--- gm4.py
+++ gm4.py
@@ -10,7 +10,6 @@ RELEASE = "release"
 
 
 def run(cmd: list[str]) -> str:
-	print("Run", cmd)
 	return subprocess.run(cmd, capture_output=True, encoding="utf8").stdout.strip()
 
 
@@ -20,7 +19,7 @@ def build_modules(ctx: Context):
 		prefix = version_meta["patch_prefix"]
 		version = version_meta["version"]
 
-	modules = [{"id": p.name} for p in ctx.directory.glob("gm4_*")]
+	modules = [{"id": p.name} for p in sorted(ctx.directory.glob("gm4_*"))]
 	print(f"[GM4] Found {len(modules)} modules")
 
 	head = run(["git", "rev-parse", "HEAD"])
@@ -33,26 +32,39 @@ def build_modules(ctx: Context):
 		released_modules = []
 		last_commit = None
 
-	print(f"version={version}, HEAD={head}, last={last_commit}")
+	print(f"version={version} HEAD={head} last={last_commit}")
 	for module in modules:
 		id = module["id"]
 		if last_commit:
 			module["diff"] = run(["git", "diff", f"{last_commit}..HEAD", "--shortstat", "--", BASE, id])
-			print(f"Gotten the diff: {module['diff']}")
 		else:
 			module["diff"] = True
-		print(f"Module {id}: {module['diff']}")
 
-	for module in modules:
-		id = module["id"]
+		released: dict | None = next((m for m in released_modules if m["id"] == id), None)
+		if not module["diff"] and released:
+			module.update(released)
+			print(f"Keeping {id}, no changes")
+			continue
+
+		module["patch"] = released["patch"] + 1 if released else prefix
 
 		try:
 			with open(f"{id}/pack.mcmeta", "r") as f:
 				meta: dict = json.load(f)
+				module["name"] = meta.get("module_name", id)
+				module["description"] = meta.get("site_description", "")
+				module["categories"] = meta.get("site_categories", [])
+				module["hidden"] = meta.get("hidden", False)
+				print(f"Updating {id}: {module['patch']}")
 		except:
 			module["id"] = None
-			continue
 
+	module_updates = [{k: m[k] for k in ["id", "name", "patch"]} for m in modules if m["id"]]
+
+	for module in modules:
+		id = module["id"]
+		if not id:
+			continue
 		ctx.require(subproject({
 			"id": id,
 			"data_pack": {
@@ -60,22 +72,16 @@ def build_modules(ctx: Context):
 				"load": [BASE, id],
 				"zipped": True,
 			},
-			"output": OUTPUT
+			"output": OUTPUT,
+			"pipeline": [
+				"gm4.module_updates"
+			],
+			"meta": {
+				"id": id,
+				"module_updates": module_updates
+			}
 		}))
-
-		released: dict | None = next((m for m in released_modules if m["id"] == id), None)
-		if not module["diff"] and released:
-			print(f"Keeping {id}, no changes")
-			module.update(released)
-			continue
-
-		patch = released["patch"] if released else prefix
-		module["name"] = meta.get("module_name", id)
-		module["description"] = meta.get("site_description", "")
-		module["categories"] = meta.get("site_categories", [])
-		module["hidden"] = meta.get("hidden", False)
-		module["patch"] = patch + 1
-		print(f"Updated {id}")
+		print(f"Generated {id}")
 
 	os.makedirs(OUTPUT, exist_ok=True)
 	with open(f"{OUTPUT}/meta.json", "w") as f:
@@ -85,3 +91,12 @@ def build_modules(ctx: Context):
 		}
 		json.dump(out, f, indent=2)
 		f.write('\n')
+
+
+def module_updates(ctx: Context):
+	init = ctx.data.functions[f"{ctx.meta['id']}:init"]
+	init.lines.remove("#$moduleUpdateList")
+	init.lines.append('# Module update list')
+	init.lines.append('data remove storage gm4:log queue[{type:"outdated"}]')
+	for m in ctx.meta["module_updates"]:
+		init.lines.append(f'execute if score {m["id"]} gm4_modules matches ..18000 run data modify storage gm4:log queue append value {{type:"outdated",module:"{m["name"]}"}}')
