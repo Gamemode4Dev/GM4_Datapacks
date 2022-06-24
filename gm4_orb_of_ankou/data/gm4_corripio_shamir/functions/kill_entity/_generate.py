dcc1adfc96996e1f17b8c59b4a69eb13f94a361b
BluePsychoRanger <40612230+BluePsychoRanger@users.noreply.github.com>
2022-06-24 13:46:27 -0700

    1.19 soul essences
    - give new 1.19 mobs soul essence drops
    - update some aquatic mobs that didn't have bubbly

--- gm4_orb_of_ankou/data/gm4_corripio_shamir/functions/kill_entity/_generate.py
+++ gm4_orb_of_ankou/data/gm4_corripio_shamir/functions/kill_entity/_generate.py
@@ -1,8 +1,8 @@
-values = ['axolotl','bat','bee','blaze', 'cat','cave_spider','chicken','cod','cow','creeper','dolphin','donkey','drowned','elder_guardian','ender_dragon','enderman','endermite','evoker','fox','ghast','giant','glow_squid','goat','guardian','hoglin','horse','husk','illusioner','iron_golem','llama','magma_cube','mooshroom','mule','ocelot','panda','parrot','phantom','pig','piglin','piglin_brute','pillager','polar_bear','pufferfish','rabbit','ravager','salmon','sheep','shulker','silverfish','skeleton_horse','skeleton','slime','snow_golem','spider','squid','stray','strider','trader_llama','tropical_fish','turtle','vex','villager','vindicator','wandering_trader','witch','wither_skeleton','wither','wolf','zoglin','zombie_horse','zombie_villager','zombie','zombified_piglin']
+values = ['allay','axolotl','bat','bee','blaze', 'cat','cave_spider','chicken','cod','cow','creeper','dolphin','donkey','drowned','elder_guardian','ender_dragon','enderman','endermite','evoker','fox','frog','ghast','giant','glow_squid','goat','guardian','hoglin','horse','husk','illusioner','iron_golem','llama','magma_cube','mooshroom','mule','ocelot','panda','parrot','phantom','pig','piglin','piglin_brute','pillager','polar_bear','pufferfish','rabbit','ravager','salmon','sheep','shulker','silverfish','skeleton_horse','skeleton','slime','snow_golem','spider','squid','stray','strider','tadpole','trader_llama','tropical_fish','turtle','vex','villager','vindicator','wandering_trader','warden','witch','wither_skeleton','wither','wolf','zoglin','zombie_horse','zombie_villager','zombie','zombified_piglin']
 # for each item in the list of items
 for value in values:
 # construct the filename; prefix or suffix optional
-    filename = value + '.mcfunction'
+    filename = './gm4_orb_of_ankou/data/gm4_corripio_shamir/functions/kill_entity/' + value + '.mcfunction'
 # open the file to be written
     fo = open(filename, 'w')
 # write the content in the file including the value being passed to each; %s indicates a string
---
+++ gm4_orb_of_ankou/data/gm4_corripio_shamir/functions/kill_entity/tadpole.mcfunction
@@ -0,0 +1,7 @@
+# run from advancement hurt_entity/tadpole
+# @s = player who hurt a tadpole using the corripio shamir
+
+loot spawn ~ ~.3 ~ fish gm4_corripio_shamir:entities/tadpole ~ ~ ~ mainhand
+execute as @e[type=item,distance=..1,nbt={Age:0s,Item:{tag:{gm4_orb_of_ankou:{item:"soul_essence"}}}}] run data merge entity @s {PickupDelay:0}
+
+advancement revoke @s only gm4_corripio_shamir:kill_entity/tadpole
