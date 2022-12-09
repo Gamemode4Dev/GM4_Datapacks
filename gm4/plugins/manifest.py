09134cff7e600da71e46206ff62dcb225a1eb8d8
Misode <Misoloo64@gmail.com>
2022-12-09 19:52:49 +0100

    Group and rename plugins

--- gm4/plugins/manifest.py
+++ gm4/plugins/manifest.py
@@ -1,4 +1,4 @@
-from beet import Context
+from beet import Context, TextFile
 from typing import Any
 import json
 import os
@@ -10,7 +10,7 @@ def run(cmd: list[str]) -> str:
 	return subprocess.run(cmd, capture_output=True, encoding="utf8").stdout.strip()
 
 
-def beet_default(ctx: Context):
+def create(ctx: Context):
 	version = os.getenv("VERSION", "1.19")
 	prefix = int(os.getenv("PATCH_PREFIX", 0))
 
@@ -72,3 +72,44 @@ def beet_default(ctx: Context):
 	with open(f'release/{version}/meta.json', 'w') as f:
 		json.dump(new_manifest, f, indent=2)
 		f.write('\n')
+
+
+def write_credits(ctx: Context):
+	manifest = ctx.cache["gm4_manifest"].json
+	contributors = manifest.get("contributors", {})
+	credits: dict[str, list[str]] = next((m["credits"] for m in manifest.get("modules", []) if m["id"] == ctx.project_id), {})
+	if credits is None or len(credits) == 0:
+		return
+
+	text = "# Credits\n"
+	for title in credits:
+		people = credits[title]
+		if not isinstance(people, list) or len(people) == 0:
+			continue
+		text += f"\n## {title}\n"
+		for p in people:
+			contributor = contributors.get(p, { "name": p })
+			name = contributor.get("name", p)
+			links: list[str] | str = contributor.get("links", [])
+			if isinstance(links, list) and len(links) >= 1:
+				text += f"- [{name}]({links[0]})\n"
+			else:
+				text += f"- {name}\n"
+	ctx.data.extra["CREDITS.md"] = TextFile(text)
+
+
+def write_updates(ctx: Context):
+	init = ctx.data.functions.get(f"{ctx.project_id}:init", None)
+	if init is None:
+		return
+
+	if "#$moduleUpdateList" in init.lines:
+		init.lines.remove("#$moduleUpdateList")
+
+	manifest = ctx.cache["gm4_manifest"].json
+	modules = manifest["modules"]
+
+	init.lines.append("# Module update list")
+	init.lines.append("data remove storage gm4:log queue[{type:'outdated'}]")
+	for m in modules:
+		init.lines.append(f"execute if score {m['id'].removeprefix('gm4_')} gm4_modules matches ..{m['patch'] - 1} run data modify storage gm4:log queue append value {{type:'outdated',module:'{m['name']}'}}")
--- gm4/plugins/output.py
+++ gm4/plugins/output.py
@@ -8,3 +8,18 @@ def beet_default(ctx: Context):
 		path=f"out/{ctx.project_id}_{version}",
 		overwrite=True,
 	)
+
+
+def release(ctx: Context):
+	version = os.getenv("VERSION", "1.19")
+	base_path = f"release/{version}/{ctx.project_id}"
+	ctx.data.save(
+		path=f"{base_path}_{version}.zip",
+		overwrite=True,
+		zipped=True,
+	)
+
+	os.makedirs(base_path, exist_ok=True)
+	for file in ["README.md", "CREDITS.md", "pack.png"]:
+		if file in ctx.data.extra:
+			ctx.data.extra[file].dump(base_path, file)
