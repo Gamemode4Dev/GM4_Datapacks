cf139df3e87a2130d8d7f5d87a2c1c748010c659
JP12 <31190190+jpeterik12@users.noreply.github.com>
2023-02-24 14:48:57 -0600

    Fix CC vanilla tags

--- lib_machines/generate_item_tags.py
+++ lib_machines/generate_item_tags.py
@@ -4,6 +4,7 @@ from beet.contrib.vanilla import Vanilla
 MCVERSION = "1.19.2"
 CC_VERSION = "3.0"
 
+
 def beet_default(ctx: Context):
   """Creates a predicate for every vanilla item tag and a function checking all of these predicates."""
   vanilla = ctx.inject(Vanilla)
@@ -29,5 +30,5 @@ def beet_default(ctx: Context):
     "# located at the center of the block",
     f"# run from gm4_custom_crafters-{CC_VERSION}:process_input/check_item via #gm4_custom_crafter:custom_item_checks",
     "",
-    *[f"execute if predicate gm4_custom_crafters-{CC_VERSION}:vanilla_item_tags/{id} run data modify storage gm4_custom_crafters:temp/crafter item.item_tags.minecraft.{id} set value 1b" for id in item_tags],
+    *[f"execute if predicate gm4_custom_crafters-{CC_VERSION}:vanilla_item_tags/{id.removeprefix('minecraft:')} run data modify storage gm4_custom_crafters:temp/crafter item.item_tags.minecraft.{id.removeprefix('minecraft:')} set value 1b" for id in item_tags],
   ])
