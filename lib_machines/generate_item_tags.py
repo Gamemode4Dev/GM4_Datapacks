d3ce5d5cca6ebfdea2958c651271f1bf4ec0143b
JP12 <31190190+jpeterik12@users.noreply.github.com>
2023-02-24 15:50:57 -0600

    fix it the smart way

--- lib_machines/generate_item_tags.py
+++ lib_machines/generate_item_tags.py
@@ -9,9 +9,9 @@ def beet_default(ctx: Context):
   """Creates a predicate for every vanilla item tag and a function checking all of these predicates."""
   vanilla = ctx.inject(Vanilla)
   item_tags = vanilla.data.item_tags
+  item_tags = [id.removeprefix("minecraft:") for id in item_tags]
 
   for id in item_tags:
-    id = id.removeprefix("minecraft:")
     ctx.data[f"gm4_custom_crafters-{CC_VERSION}:vanilla_item_tags/{id}"] = Predicate({
       "condition": "minecraft:entity_properties",
       "entity": "this",
@@ -30,5 +30,5 @@ def beet_default(ctx: Context):
     "# located at the center of the block",
     f"# run from gm4_custom_crafters-{CC_VERSION}:process_input/check_item via #gm4_custom_crafter:custom_item_checks",
     "",
-    *[f"execute if predicate gm4_custom_crafters-{CC_VERSION}:vanilla_item_tags/{id.removeprefix('minecraft:')} run data modify storage gm4_custom_crafters:temp/crafter item.item_tags.minecraft.{id.removeprefix('minecraft:')} set value 1b" for id in item_tags],
+    *[f"execute if predicate gm4_custom_crafters-{CC_VERSION}:vanilla_item_tags/{id} run data modify storage gm4_custom_crafters:temp/crafter item.item_tags.minecraft.{id} set value 1b" for id in item_tags],
   ])
