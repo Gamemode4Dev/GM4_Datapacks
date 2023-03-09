64d3fed6f28e69b4af216d5b44ef02f6b593121e
SpecialBuilder <specialbuilder32@gmail.com>
2023-03-09 17:51:09 -0500

    Module Marketing Readmes (#847)
    * Update main.yml
    
    * Initial commit for draft PR
    
    * Comments and plans
    
    - tested iframes on modrinth
    
    * Additional README tweaks
    
    * Loading of README.md and initial regex replacements
    
    First step toward replacing URLs on different platforms. Currently implemented in normal build process (beet dev) rather than in release pipeline as that throws errors on my machine.
    
    * Autofill credits into README.md
    
    - Autofills names and links in README.md in a similar format to Credits.md
    
    * Autofill YT and Wiki links into README
    
    - autofills YT links and Wiki links from links contained in beet.yaml
    
    * Autofill image URLs
    
    - Converts tagged images to point to the raw.githubcontent URL it corresponds to
    
    * Auto embed YT video for Modrinth Readme.md
    
    - pulls yt video (if present) from beet.yaml and embeds it in a readme file stored in ctx.meta, to be used when sent to modrinth
    
    * -Dynamic linking to project pages
    
    mutates links to other gm4 modules on gm4.co to point to their respective project page on sites like modrinth and smithed, where those pages exist
    
    * Add dynamic links to PMC and Smithed
    
    Adds support for PMC and Smithed link replacement, where pages are available
    
    * PMC BBCode conversions
    
    Adds (too many) regex codes to translate markdown into bbcode, in the style of our PMC posts
    
    * PMC BBCode Tables
    
    Support for translating markdown tables to bbcode. Requires extra "anchor tags" where used however, since recognizing arbitrary tables is a bit of a mess and we'll use tables infrequently in postings
    
    * Code cleanup
    
    * Readme generating command
    
    Adds a custom beet command to generate only readme files for each distribution site, for use when a new download page is being created.
    
    NOTE: currently generates a full new manifest every use. Next task is refactoring of manifest to allow for more fine-grain control of its tasks
    
    * Merge conflict error resolution
    
    * Refactor of manifest.create
    
    Splits the patch increment process to a separate function, so processes that require the manifest (for links to YT videos for example) but not the patch value don't attempt to generate new patch values
    
    -Used for the readme-gen workflow, which only reads the manifest for project page links, but does not require the patch values to be incremented as it only outputs the README files, and not the pack itself
    
    * External required files moved to `generated` directory
    
    Replaces `release/1.19/gm4_chairs/...` with `release/1.19/generated` directory, containing files required for external reference. Currently, that is the smithed readme.md, and pack.png
    
    * Only run PMC generations from the manual command
    
    PMC has no API, so the standard release task can skip those processes.
    
    * Addition of standard Header and Footer inserts
    
    * Existing READMEs to new generator format
    
    * Watch and Clean command options
    
    * Small tweaks
    
    * Header formatting

--- gm4/plugins/manifest.py
+++ gm4/plugins/manifest.py
@@ -13,12 +13,7 @@ def run(cmd: list[str]) -> str:
 
 
 def create(ctx: Context):
-	"""Collect a manifest for all modules and update their patch version if necessary."""
-	version = os.getenv("VERSION", "1.19")
-	prefix = int(os.getenv("PATCH_PREFIX", 0))
-	release_dir = Path('release') / version
-	manifest_file = release_dir / "meta.json"
-
+	"""Collect a manifest for all modules from respective beet.yaml files."""
 	modules: list[dict[str, Any]] = [{"id": p.name} for p in sorted(ctx.directory.glob("gm4_*"))]
 
 	for module in modules:
@@ -38,36 +33,15 @@ def create(ctx: Context):
 				module["hidden"] = True
 			if "notes" in meta and len(meta["notes"]) > 0:
 				module["important_note"] = meta["notes"][0]
+			module["modrinth_id"] = project_config.get("meta", {}).get("modrinth", {}).get("project_id")
+			module["smithed_link"] = project_config.get("meta", {}).get("smithed", {}).get("uid") # NOTE field to be named when smithed api v2 leaves beta
+			module["pmc_link"] = project_config.get("meta", {}).get("planetminecraft", {}).get("uid")
 			module.update()
 		else:
 			module["id"] = None
 
 	# If a module doesn't have a valid beet.yaml file don't include it
 	modules = [m for m in modules if m["id"] is not None]
-
-	if manifest_file.exists():
-		manifest = json.loads(manifest_file.read_text())
-		last_commit = manifest["last_commit"]
-		released_modules: list[dict[str, Any]] = manifest["modules"]
-	else:
-		last_commit = None
-		released_modules = []
-
-	for module in modules:
-		id = module["id"]
-
-		# Check if there are any changes between last commit and HEAD
-		diff = run(["git", "diff", last_commit, "--shortstat", "--", id]) if last_commit else True
-		released = next((m for m in released_modules if m["id"] == id), None)
-
-		if not diff and released:
-			# No changes were made, keep the same patch version
-			module["patch"] = released["patch"]
-		else:
-			# Changes were made or this is the first release, bump the patch
-			patch = released["patch"] if released else prefix
-			module["patch"] = patch + 1
-			print(f"[GM4] Updating {id} to {patch + 1}")
 	
 	# Read the contributors metadata
 	contributors_file = Path("contributors.json")
@@ -82,9 +56,46 @@ def create(ctx: Context):
 	new_manifest = {
 		"last_commit": head,
 		"modules": modules,
-		"contributors": contributors,
+		"contributors": contributors
 	}
 	ctx.cache["gm4_manifest"].json = new_manifest
+
+
+def update_patch(ctx: Context):
+	"""Retrieves manifest from previous build, and increments patch number
+	 	 if there are any changes between last commit and HEAD"""
+	version = os.getenv("VERSION", "1.19")
+	prefix = int(os.getenv("PATCH_PREFIX", 0))
+	release_dir = Path('release') / version
+	manifest_file = release_dir / "meta.json"
+
+	modules = ctx.cache["gm4_manifest"].json["modules"]
+
+	if manifest_file.exists():
+		old_manifest = json.loads(manifest_file.read_text())
+		last_commit = old_manifest["last_commit"]
+		released_modules: list[dict[str, Any]] = old_manifest["modules"]
+	else:
+		last_commit = None
+		released_modules = []
+
+	# Check if there are any changes between last commit and HEAD
+	for module in modules:
+		id = module["id"]
+
+		diff = run(["git", "diff", last_commit, "--shortstat", "--", id]) if last_commit else True
+		released = next((m for m in released_modules if m["id"] == id), None)
+
+		if not diff and released:
+			# No changes were made, keep the same patch version
+			module["patch"] = released["patch"]
+		else:
+			# Changes were made or this is the first release, bump the patch
+			patch = released["patch"] if released else prefix
+			module["patch"] = patch + 1
+			print(f"[GM4] Updating {id} to {patch + 1}")
+
+	ctx.cache["gm4_manifest"].json["modules"] = modules
 
 
 def write_meta(ctx: Context):
@@ -99,28 +110,38 @@ def write_meta(ctx: Context):
 
 
 def write_credits(ctx: Context):
-	"""Writes the credits metadata to CREDITS.md."""
+	"""Writes the credits metadata to CREDITS.md. and collects for README.md"""
 	manifest = ctx.cache["gm4_manifest"].json
 	contributors = manifest.get("contributors", {})
 	credits: dict[str, list[str]] = next((m["credits"] for m in manifest.get("modules", []) if m["id"] == ctx.project_id), {})
 	if credits is None or len(credits) == 0:
 		return
 
-	text = "# Credits\n"
+	# traverses contributors and associates name with links for printing
+	linked_credits: dict[str, list[str]] = {}
 	for title in credits:
 		people = credits[title]
 		if not isinstance(people, list) or len(people) == 0:
 			continue
-		text += f"\n## {title}\n"
+		linked_credits[title] = []
 		for p in people:
 			contributor = contributors.get(p, { "name": p })
 			name = contributor.get("name", p)
 			links: list[str] | str = contributor.get("links", [])
 			if isinstance(links, list) and len(links) >= 1:
-				text += f"- [{name}]({links[0]})\n"
+				linked_credits[title].append(f"[{name}]({links[0]})")
 			else:
-				text += f"- {name}\n"
+				linked_credits[title].append(f"{name}")
+	
+	# format credits for CREDITS.md
+	text = "# Credits\n"
+	for title in linked_credits:
+		text += f"\n## {title}\n"
+		for link in linked_credits[title]:
+			text += f'- {link}\n'
+
 	ctx.data.extra["CREDITS.md"] = TextFile(text)
+	ctx.meta['linked_credits'] = linked_credits # pass data to README portion of pipeline
 
 
 def write_updates(ctx: Context):
