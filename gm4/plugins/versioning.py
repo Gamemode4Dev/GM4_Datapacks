fd1651d4abe9d02de1aa77600ac2aa55c07fb736
SpecialBuilder <specialbuilder32@gmail.com>
2023-03-26 15:05:06 -0400

    Correct schedule_loops config read

--- gm4/plugins/versioning.py
+++ gm4/plugins/versioning.py
@@ -58,7 +58,7 @@ def modules(ctx: Context):
     lines.append(f"execute if score {ctx.project_id} load.status matches {module_ver.major} run function {ctx.project_id}:init")
 
     # unschedule clocks
-    for function in ctx.meta["gm4"].get("schedule_loops", []):
+    for function in ctx.meta["gm4"].get("versioning", {}).get("schedule_loops", []):
         namespaced_function = f"{ctx.project_id}:{function}" if ":" not in function else function
         lines.append(f"execute unless score {ctx.project_id} load.status matches {module_ver.major} run schedule clear {namespaced_function}")
 
@@ -110,7 +110,7 @@ def libraries(ctx: Context):
     # resolve_load.mcfunction
     lines = [f"execute if score {ctx.project_id} load.status matches {lib_ver.major} if score {ctx.project_id}_minor load.status matches {lib_ver.minor} run function {ctx.project_id}:load"]
 
-    for func in ctx.meta["gm4"].get("schedule_loops", []):
+    for func in ctx.meta["gm4"].get("versioning", {}).get("schedule_loops", []):
         lines.append(f"execute unless score {ctx.project_id} load.status matches {lib_ver.major} run schedule clear {ctx.project_id}:{func}")
         lines.append(f"execute unless score {ctx.project_id}_minor load.status matches {lib_ver.minor} run schedule clear {ctx.project_id}:{func}")
         
