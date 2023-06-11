f6d6a1351ad8eb95d703848ebad406408c7c3b77
SpecialBuilder <specialbuilder32@gmail.com>
2023-06-11 13:49:44 -0400

    Extend plugin
    Plugin that allows for extension of other config files but with passing necessary metadata into the subproject

--- gm4/plugins/extend.py
+++ gm4/plugins/extend.py
@@ -2,11 +2,17 @@ from beet import Context, subproject
 from pathlib import Path
 import yaml
 
-def module(ctx: Context):
-    """Extends the module.yaml project config within a pipeline without losing necessary parent project context"""
-    with open(Path("module.yaml")) as f:
-        conf = yaml.safe_load(f)
-    for p in ['id', 'name', 'version']:
-        conf[p] = ctx.__getattribute__(f'project_{p}')
-    conf['meta'] = {**ctx.meta, **conf['meta']} # module.yaml meta prioritized
-    ctx.require(subproject(conf))
+def _generate_extend_plugin(name: str):
+    def plugin(ctx: Context):
+        """Extends the specified project config within a pipeline without losing necessary parent context
+            - necessary to run "boilerplate" plugins in the middle of a build pipeline"""
+        with open(Path(name)) as f:
+            conf = yaml.safe_load(f)
+        for p in ['id', 'name', 'version']:
+            conf[p] = ctx.__getattribute__(f'project_{p}')
+        conf['meta'] = {**ctx.meta, **conf.get('meta', {})} # extended-yaml meta prioritized over parent
+        ctx.require(subproject(conf))
+    return plugin
+
+library = _generate_extend_plugin('library.yaml')
+module = _generate_extend_plugin('module.yaml')
