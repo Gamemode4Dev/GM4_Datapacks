d075a4cdfd987ec5d57c0eac3cf6ada7a9b549b3
SpecialBuilder <specialbuilder32@gmail.com>
2023-07-02 00:30:46 -0400

    Recursive json-dict key retrieval

--- gm4/utils.py
+++ gm4/utils.py
@@ -1,6 +1,7 @@
 import subprocess
 import warnings
 from dataclasses import dataclass
+from typing import Any
 
 def run(cmd: list[str]) -> str:
 	"""Run a shell command and return the stdout."""
@@ -30,3 +31,18 @@ class Version():
 		if type(None) in map(type, [self.major, self.minor, self.patch]):
 			raise TypeError(f"Version number cannot be converted to integer when one or more fields are not set")
 		return 100_000*self.major + 1_000*self.minor + self.patch # type: ignore
+	
+def nested_get(d: dict[str, Any], key: str) -> list[Any]:
+	"""Recursively traverses a string-keyed dict (like minecraft json files) for the specified key, returning all that exist
+		returns empty list and throws no errors if key does not exist"""
+	ret_list: list[Any] = []
+	for k, v in d.items():
+		if k == key:
+			ret_list.append(v)
+		elif type(v) is dict:
+			ret_list.extend(nested_get(v, key)) # type: ignore
+		elif type(v) is list:
+			for elem in v:  # type: ignore
+				if type(elem) is dict:  # type: ignore
+					ret_list.extend(nested_get(elem, key))  # type: ignore
+	return ret_list
