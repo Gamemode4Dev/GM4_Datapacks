b7bfe1839453aba656d0c9889abfa8ebb35801a7
SpecialBuilder <specialbuilder32@gmail.com>
2023-08-17 19:28:29 -0400

    Move index retrieval/assignment to class methods

--- gm4/plugins/resource_pack.py
+++ gm4/plugins/resource_pack.py
@@ -8,6 +8,7 @@ from functools import partial, cached_property
 from beet.contrib.vanilla import Vanilla
 from gm4.utils import add_namespace
 import logging
+from mecha import Diagnostic
 
 CUSTOM_MODEL_PREFIX = 3420000
 
@@ -92,7 +93,7 @@ class GM4ResourcePack():
 
     def __init__(self, ctx: Context):
         self.ctx = ctx
-        self.registry_file = JsonFile(source_path="gm4/modeldata_registry.json") # TODO caching/save/output this
+        self.registry = JsonFile(source_path="gm4/modeldata_registry.json").data # TODO caching/save/output this
 
     @cached_property
     def opts(self) -> FlatModelDataOptions:
@@ -101,27 +102,23 @@ class GM4ResourcePack():
 
     def update_modeldata_registry(self):
         """Updates shared modeldata_registry.json with entries from the beet.yaml"""
-        logger = parent_logger.getChild(f"update_modeldata_registry.{self.ctx.project_id}")
-        item_registry: dict[str, dict[str, int]] = self.registry_file.data["items"]
+        logger = parent_logger.getChild(f"update_modeldata_registry.{self.ctx.project_id}") # FIXME logger for class?
+        item_registry: dict[str, dict[str, int]] = self.registry.setdefault("items", {})
 
         # add new references and assign values
         for m in self.opts.model_data:
             ref = add_namespace(m.reference, self.ctx.project_id)
-            i = _retrieve_index(ref)
-            if i is not None: # existing index, is it available to assign to all items
-                conflicts = False
+            conflicts = False
+            i, err = self.retrieve_index(ref)
+            if not err: # existing index, is it available to assign to all items?
                 for item_id in m.item.entries():
                     reg = item_registry[item_id]
                     used_idxs = {k: reg[k] for k in reg.keys() - {ref}}.values()
                     if i in used_idxs:
                         logger.warning(f"Failed to share existing CustomModelData for '{ref}' to '{item_id}'. A new value will be assigned for this reference; existing items may lose their texture!")
                         conflicts = True
-            if i is None or conflicts: # no existing index, or existing isn't available; get a new one
-                i = _retrieve_new_index(m.item.entries(), self.ctx.project_id)
-            for item_id in m.item.entries():
-                if ref not in item_registry.setdefault(item_id, {}):
-                    item_registry[item_id][ref] = i
-                    logger.info(f"Issuing new CustomModelData for '{ref}': {i}") # FIXME issues with adding a new item to existing registry
+            if err or conflicts: # no existing index, or existing isn't available; get a new one
+                self.assign_new_index(m.item.entries(), ref)
 
         # remove unused references
             # NOTE deleting modeldata is really only supported for development cycles. Once published, a cmd value should be permanent.
@@ -145,12 +142,10 @@ class GM4ResourcePack():
     def generate_model_overrides(self):
         """Generates item model overrides in the 'minecraft' namespace, adding predicates for CustomModelData"""
         
-        registry = self.registry_file.data # FIXME
         vanilla = self.ctx.inject(Vanilla)
         vanilla_models_jar = vanilla.mount("assets/minecraft/models/item")
-
        
-        # sort models by item id
+        # group models by item id
         for item_id in {i for m in self.opts.model_data for i in m.item.entries()}:
             models = list(filter(lambda m: item_id in m.item.entries(), self.opts.model_data))
             print(f"{item_id} models: {models}")
@@ -160,14 +155,14 @@ class GM4ResourcePack():
             print(f"{item_id} overrides start: {overrides}")
 
             filter_func = partial(_filter_by_reference, models=models, namespace=self.ctx.project_id)
-            custom_model_data = dict(filter(filter_func, registry["items"][item_id].items())) # TODO error logging
+            custom_model_data = dict(filter(filter_func, self.registry["items"][item_id].items())) # TODO error logging
             
             for model in models:
                 # setup overrides to add CMD to
                 merge_overrides = [] # FIXME this shouldnt need to be initialized
                 if isinstance(model.model, list): # manual predicate merging specified
                     merge_overrides = model.model # FIXME check branch unnecessary
-                elif isinstance(model.model, str): 
+                else: 
                     merge_overrides = overrides.copy() # get vanilla overrides # FIXME deepcopy?
                 if len(merge_overrides) == 0:
                     merge_overrides.append({}) # add an empty predicate to add CMD onto
@@ -184,6 +179,36 @@ class GM4ResourcePack():
             print(overrides)
             self.ctx.assets.models[f"minecraft:item/{item_id}"] = Model(model_override) # TODO skipped-values spacing, on RP output after merge :)
 
+    def retrieve_index(self, reference: str) -> tuple[int, KeyError|Diagnostic|None]:
+        """retrieves the CMD value for the given reference"""
+        for reg in self.registry["items"].values():
+            if reference in reg:
+                return reg[reference], None
+        return -CUSTOM_MODEL_PREFIX, KeyError(f"{reference} has no asscioated index") # TODO make this Diagnostic
+    
+    def assign_new_index(self, item_ids: list[str], reference: str):
+        """finds the next available CMD value for the given items and applies it to the registry"""
+        l, u = self.registry["allocations"].get(self.ctx.project_id, (0,99)) # FIXME what happens when the default allocation fills up
+        logger = parent_logger.getChild("assign_new_index") # TODO better logger structure
+        available_indices = set(range(l, u+1))
+
+        for item_id in item_ids:
+            used_values = set(self.registry["items"].get(item_id, {}).values())
+            available_indices -= used_values
+
+        if not available_indices:
+            parent_logger.warning("No Valid CMD is open for assignment!") # FIXME this warn
+            raise RuntimeError("ran out of CMD to assign") # FIXME
+        
+        i = min(available_indices)
+    
+        item_registry = self.registry.setdefault("items", {})
+        for item_id in item_ids:
+            if reference not in item_registry.setdefault(item_id, {}):
+                item_registry[item_id][reference] = i
+                logger.info(f"Issuing new CustomModelData for '{reference}': {i}")
+        
+
 def _filter_by_reference(registry_itemview: tuple[str, int], models: list[ModelData], namespace: str) -> bool:
     """a filter checking if an entriy of the cmd registry matches a given list of models"""
     registry_ref, index = registry_itemview
@@ -197,26 +222,4 @@ def _filter_by_reference(registry_itemview: tuple[str, int], models: list[ModelD
             return True
     return False
 
-def _retrieve_new_index(item_ids: list[str], project_id: str) -> int:
-    """finds the next available CMD value for the given model"""
-    registry = JsonFile(source_path="gm4/modeldata_registry.json").data # TODO this might be a service object member 
-    l, u = registry["allocations"].get(project_id, (0,99)) # FIXME what happens when the default allocation fills up
-    available_indices = set(range(l, u+1))
 
-    for item_id in item_ids:
-        used_values = set(registry["items"].get(item_id, {}).values())
-        available_indices -= used_values
-
-    if not available_indices:
-        parent_logger.warning("No Valid CMD is open for assignment!") # FIXME this warn
-        return None
-    return min(available_indices)
-
-def _retrieve_index(reference: str) -> int:
-    """retrieves the CMD value for the given reference""" # TODO move to service objecT? rename this function?
-    registry = JsonFile(source_path="gm4/modeldata_registry.json").data
-
-    for item_id, reg in registry["items"].items():
-        if reference in reg:
-            return reg[reference]
-    return None # TODO this becomes an exception? KeyError
