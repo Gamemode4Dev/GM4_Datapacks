19cc2d8d2a27ba07b5bb46b753f4133191c40717
SpecialBuilder <specialbuilder32@gmail.com>
2023-08-17 12:52:28 -0400

    Transition existing processes to service object

--- gm4/plugins/resource_pack.py
+++ gm4/plugins/resource_pack.py
@@ -41,95 +41,107 @@ class ModelDataOptions(PluginOptions, extra=Extra.ignore):
             new_data.extend(model.collapse_broadcast())
         self.model_data = new_data
 
-@configurable(name="gm4", validator=ModelDataOptions)
-def update_modeldata_registry(ctx: Context, opts: ModelDataOptions):
-    """Updates shared modeldata_registry.json with entries from the beet.yaml"""
-    registry_file = JsonFile(source_path="gm4/modeldata_registry.json")
-    logger = parent_logger.getChild(f"update_modeldata_registry.{ctx.project_id}")
-    item_registry: dict[str, dict[str, int]] = registry_file.data["items"]
-    opts.process_inheritance()
+def update_modeldata_registry(ctx: Context): # TEMP redirect
+    o = ctx.inject(GM4ResourcePack)
+    o.update_modeldata_registry()
+def generate_model_overrides(ctx: Context): # TEMP redirect
+    o = ctx.inject(GM4ResourcePack)
+    o.generate_model_overrides()
 
-    # add new references and assign values
-    for m in opts.model_data:
-        ref = add_namespace(m.reference, ctx.project_id)
-        i = _retrieve_index(ref)
-        if i is not None: # existing index, is it available to assign to all items
-            conflicts = False
+class GM4ResourcePack():
+    """Service Object handling CustomModelData and generated item models"""
+
+    def __init__(self, ctx: Context):
+        self.ctx = ctx
+        self.opts = ctx.validate("gm4", validator=ModelDataOptions) # TODO process inheritance?
+        self.registry_file = JsonFile(source_path="gm4/modeldata_registry.json")
+
+    def update_modeldata_registry(self):
+        """Updates shared modeldata_registry.json with entries from the beet.yaml"""
+        logger = parent_logger.getChild(f"update_modeldata_registry.{self.ctx.project_id}")
+        item_registry: dict[str, dict[str, int]] = self.registry_file.data["items"]
+        self.opts.process_inheritance()
+
+        # add new references and assign values
+        for m in self.opts.model_data:
+            ref = add_namespace(m.reference, self.ctx.project_id)
+            i = _retrieve_index(ref)
+            if i is not None: # existing index, is it available to assign to all items
+                conflicts = False
+                for item_id in m.item.entries():
+                    reg = item_registry[item_id]
+                    used_idxs = {k: reg[k] for k in reg.keys() - {ref}}.values()
+                    if i in used_idxs:
+                        logger.warning(f"Failed to share existing CustomModelData for '{ref}' to '{item_id}'. A new value will be assigned for this reference; existing items may lose their texture!")
+                        conflicts = True
+            if i is None or conflicts: # no existing index, or existing isn't available; get a new one
+                i = _retrieve_new_index(m.item.entries(), self.ctx.project_id)
             for item_id in m.item.entries():
-                reg = item_registry[item_id]
-                used_idxs = {k: reg[k] for k in reg.keys() - {ref}}.values()
-                if i in used_idxs:
-                    logger.warning(f"Failed to share existing CustomModelData for '{ref}' to '{item_id}'. A new value will be assigned for this reference; existing items may lose their texture!")
-                    conflicts = True
-        if i is None or conflicts: # no existing index, or existing isn't available; get a new one
-            i = _retrieve_new_index(m.item.entries(), ctx.project_id)
-        for item_id in m.item.entries():
-            if ref not in item_registry.setdefault(item_id, {}):
-                item_registry[item_id][ref] = i
-                logger.info(f"Issuing new CustomModelData for '{ref}': {i}") # FIXME issues with adding a new item to existing registry
+                if ref not in item_registry.setdefault(item_id, {}):
+                    item_registry[item_id][ref] = i
+                    logger.info(f"Issuing new CustomModelData for '{ref}': {i}") # FIXME issues with adding a new item to existing registry
 
-    # remove unused references
-        # NOTE deleting modeldata is really only supported for development cycles. Once published, a cmd value should be permanent.
-        # Thus, a reference will only be removed if it is no longer present on *any* item in the beet.yaml
-    all_refs = {v for r in opts.model_data if (v:=add_namespace(r.reference, ctx.project_id)).startswith(ctx.project_id)}
-    for item_id, reg in item_registry.items():
-        for ref in list(reg.keys()):
-            if ref.startswith(ctx.project_id) and ref not in all_refs:
-                logger.info(f"Removing undefined CustomModelData from registry: '{ref}'")
-                del reg[ref]
-        #FIXME clear references from items no longer configured too
+        # remove unused references
+            # NOTE deleting modeldata is really only supported for development cycles. Once published, a cmd value should be permanent.
+            # Thus, a reference will only be removed if it is no longer present on *any* item in the beet.yaml
+        all_refs = {v for r in self.opts.model_data if (v:=add_namespace(r.reference, self.ctx.project_id)).startswith(self.ctx.project_id)}
+        for item_id, reg in item_registry.items():
+            for ref in list(reg.keys()):
+                if ref.startswith(self.ctx.project_id) and ref not in all_refs:
+                    logger.info(f"Removing undefined CustomModelData from registry: '{ref}'")
+                    del reg[ref]
+            #FIXME clear references from items no longer configured too
 
-    # sort registriy alphabetically and numerically # TODO this as a cleanup step?
-    registry_file.data["items"] = dict(sorted(registry_file.data["items"].items()))
-    for item_id, ref_map in item_registry.items():
-        item_registry[item_id] = dict(sorted(ref_map.items(), key=lambda e: e[1]))
+        # # sort registriy alphabetically and numerically # TODO this as a cleanup step?
+        # self.registry_file.data["items"] = dict(sorted(registry_file.data["items"].items()))
+        # for item_id, ref_map in item_registry.items():
+        #     item_registry[item_id] = dict(sorted(ref_map.items(), key=lambda e: e[1]))
 
-    registry_file.dump(origin="", path="gm4/modeldata_registry.json") # TODO cache this file somehow? Part of RP service exit?
+        # self.registry_file.dump(origin="", path="gm4/modeldata_registry.json") # TODO cache this file somehow? Part of RP service exit?
 
 
-# TODO move this to a service object to prevent re-creating the meta config or inheritance process?
-@configurable(name="gm4", validator=ModelDataOptions)
-def generate_model_overrides(ctx: Context, opts: ModelDataOptions):
-    """Generates item model overrides in the 'minecraft' namespace, adding predicates for CustomModelData"""
-
-    registry = JsonFile(source_path="gm4/modeldata_registry.json").data
-    vanilla = ctx.inject(Vanilla)
-    vanilla_models_jar = vanilla.mount("assets/minecraft/models/item")
-
-    opts.process_inheritance()
-    
-    # sort models by item id
-    for item_id in {i for m in opts.model_data for i in m.item.entries()}:
-        models = list(filter(lambda m: item_id in m.item.entries(), opts.model_data))
-        print(f"{item_id} models: {models}")
-
-        model_override = (v:=vanilla_models_jar.assets.models[f"minecraft:item/{item_id}"].data) | ({} if v.get("overrides") else {"overrides": []})
-        overrides = model_override["overrides"]
-        print(f"{item_id} overrides start: {overrides}")
-
-        filter_func = partial(_filter_by_reference, models=models, namespace=ctx.project_id)
-        custom_model_data = dict(filter(filter_func, registry["items"][item_id].items())) # TODO error logging
+    def generate_model_overrides(self):
+        """Generates item model overrides in the 'minecraft' namespace, adding predicates for CustomModelData"""
         
-        for model in models:
-            # setup overrides to add CMD to
-            if isinstance(model.model, list): # manual predicate merging specified
-                merge_overrides = model.model # FIXME check branch unnecessary
-            elif isinstance(model.model, str): 
-                merge_overrides = overrides.copy() # get vanilla overrides # FIXME deepcopy?
-            if len(merge_overrides) == 0:
-                merge_overrides.append({}) # add an empty predicate to add CMD onto
+        registry = self.registry_file.data # FIXME
+        vanilla = self.ctx.inject(Vanilla)
+        vanilla_models_jar = vanilla.mount("assets/minecraft/models/item")
 
-            ref = add_namespace(model.reference, ctx.project_id)
+        self.opts.process_inheritance()
+        
+        # sort models by item id
+        for item_id in {i for m in self.opts.model_data for i in m.item.entries()}:
+            models = list(filter(lambda m: item_id in m.item.entries(), self.opts.model_data))
+            print(f"{item_id} models: {models}")
 
-            for pred in merge_overrides:
-                overrides.append({
-                    "predicate": {
-                        "custom_model_data": CUSTOM_MODEL_PREFIX+custom_model_data[ref]
-                    } | pred.get("predicate", {}),
-                    "model": ref #model.model # FIXME points to generated model file?
-                })
-        print(overrides)
-        ctx.assets.models[f"minecraft:item/{item_id}"] = Model(model_override) # TODO skipped-values spacing, on RP output after merge :)
+            model_override = (v:=vanilla_models_jar.assets.models[f"minecraft:item/{item_id}"].data) | ({} if v.get("overrides") else {"overrides": []})
+            overrides = model_override["overrides"]
+            print(f"{item_id} overrides start: {overrides}")
+
+            filter_func = partial(_filter_by_reference, models=models, namespace=self.ctx.project_id)
+            custom_model_data = dict(filter(filter_func, registry["items"][item_id].items())) # TODO error logging
+            
+            for model in models:
+                # setup overrides to add CMD to
+                merge_overrides = [] # FIXME this shouldnt need to be initialized
+                if isinstance(model.model, list): # manual predicate merging specified
+                    merge_overrides = model.model # FIXME check branch unnecessary
+                elif isinstance(model.model, str): 
+                    merge_overrides = overrides.copy() # get vanilla overrides # FIXME deepcopy?
+                if len(merge_overrides) == 0:
+                    merge_overrides.append({}) # add an empty predicate to add CMD onto
+
+                ref = add_namespace(model.reference, self.ctx.project_id)
+
+                for pred in merge_overrides:
+                    overrides.append({
+                        "predicate": {
+                            "custom_model_data": CUSTOM_MODEL_PREFIX+custom_model_data[ref]
+                        } | pred.get("predicate", {}),
+                        "model": ref #model.model # FIXME points to generated model file?
+                    })
+            print(overrides)
+            self.ctx.assets.models[f"minecraft:item/{item_id}"] = Model(model_override) # TODO skipped-values spacing, on RP output after merge :)
 
 def _filter_by_reference(registry_itemview: tuple[str, int], models: list[ModelData], namespace: str) -> bool:
     """a filter checking if an entriy of the cmd registry matches a given list of models"""
