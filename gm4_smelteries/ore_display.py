8a6fd735c882efac2a50058c25970f5cebf6187d
SpecialBuilder <specialbuilder32@gmail.com>
2023-10-17 02:37:35 -0400

    Smelteries models
    NOTE: This uses the expandability function of the item model templates, although it would be roughly as easy to implement programmatically in a generator plugin

---
+++ gm4_smelteries/ore_display.py
@@ -0,0 +1,29 @@
+from beet import NamespaceProxy, Model, Context
+from gm4.plugins.resource_pack import TemplateBase, ModelData, ItemDisplayModel
+
+class OreDisplayTemplate(TemplateBase):
+    """setup a model template for the smeltable ores displayed by smeltries"""
+    name = "smelteries:ore_display"
+    texture_map = ["top", "side"]
+    
+    default_transforms = [
+        ItemDisplayModel(
+            origin=[0.5, 0.5, 0.5],
+            scale=[0.624, 0.624, 0.624],
+            translation=[0, 0.19, 0]
+        )
+    ]
+
+    @staticmethod
+    def process(config: ModelData, models_container: NamespaceProxy[Model]):
+        reference = config.reference.split('/')[-1]
+        return Model({
+            "parent": "gm4_smelteries:block/ore_display",
+            "textures":{
+                "top": f"gm4_smelteries:block/ore_display/{reference}_top",
+                "side": f"gm4_smelteries:block/ore_display/{reference}_side",
+            }
+        })
+
+def beet_default(ctx: Context):
+    pass
