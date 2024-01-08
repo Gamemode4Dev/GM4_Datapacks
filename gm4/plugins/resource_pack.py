04c18a24374da4c2e5ac9fd056b8486476148687
SpecialBuilder <specialbuilder32@gmail.com>
2024-01-08 17:26:26 -0500

    `generated` template supports creating the same model for multiple items

--- gm4/plugins/resource_pack.py
+++ gm4/plugins/resource_pack.py
@@ -705,14 +705,22 @@ class GeneratedTemplate(TemplateOptions):
     name = "generated"
 
     def process(self, config: ModelData, models_container: NamespaceProxy[Model]) -> list[Model]:
-        model_name = ensure_single_model_config(self.name, config)
-        m = models_container[model_name] = Model({
-            "parent": "minecraft:item/generated",
-            "textures": {
-                "layer0": f"{config.textures.entries()[0]}"
-            }
-        })
-        return [m]
+        if len(config.textures.entries()) > 1:
+            raise InvalidOptions("gm4.model_data", f"{config.reference}; Template 'generated' currently only supports a single texture.")
+            # NOTE in the future, `generated` could accept a map for textures to provide a different texture for each model.        
+        
+        ret_list: list[Model] = []
+        for model_name in config.model.entries():
+            if isinstance(model_name, list):
+                raise InvalidOptions("gm4.model_data", f"{config.reference}; Template 'generated' does not support predicate override 'model' fields.")
+            m = models_container[model_name] = Model({
+                "parent": "minecraft:item/generated",
+                "textures": {
+                    "layer0": f"{config.textures.entries()[0]}"
+                }
+            })
+            ret_list.append(m)
+        return ret_list
     
 class GeneratedOverlayTemplate(TemplateOptions):
     name = "generated_overlay"
