6fa23a71b557bb2dfcf99eeb4b03977b704191db
SpecialBuilder <specialbuilder32@gmail.com>
2024-01-13 14:17:07 -0500

    Restore CMD model padding

--- gm4/plugins/autoload.py
+++ gm4/plugins/autoload.py
@@ -3,6 +3,9 @@ from beet import Context
 def beet_default(ctx: Context):
     """Loads plugins auto-added to every beet subproject. Configured in pyproject.toml
     Used primarily to load custom-merge-rules that will apply between broadcast packs, at least until a better option is available in beet"""
+    ctx.meta["model_merging"] = {
+        "predicate_order": ["custom_model_data"]  # set default model_merging config for these broadcast-created subprojects
+    }
     ctx.require(
         "beet.contrib.default",
         "beet.contrib.model_merging",
