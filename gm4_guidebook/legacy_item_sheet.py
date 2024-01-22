a06d203cd83710255bae3697877dc739cc878fb0
SpecialBuilder <specialbuilder32@gmail.com>
2024-01-22 16:36:47 -0500

    Add copies of legacy guidebook gm4 items

--- gm4_guidebook/legacy_item_sheet.py
+++ gm4_guidebook/legacy_item_sheet.py
@@ -4,4 +4,6 @@ def beet_default(ctx: Context):
     """Merges the `guidebook_items.json Font with the master `guidebook.json`.
     This is the temporary-ish organization technique until our custom models/items can be rendered on demand and formed into a spritesheet"""
 
-    ctx.assets.fonts["gm4:guidebook"].merge( ctx.assets.fonts["gm4:guidebook_items"] )
+    # NOTE these font files will be auto-generated in the future
+    ctx.assets.fonts["gm4:guidebook"].merge( ctx.assets.fonts["gm4:gm4_items"] )
+    ctx.assets.fonts["gm4:guidebook"].merge( ctx.assets.fonts["gm4:vanilla_items"] )
