da72352e81b0698280fcf0e15976b799f3e76ed3
SpecialBuilder <specialbuilder32@gmail.com>
2024-02-12 11:44:24 -0500

    Skip mecha linting in beet-test.yaml

--- gm4/plugins/test.py
+++ gm4/plugins/test.py
@@ -1,5 +1,6 @@
 from beet import Context, TextFile
 from typing import ClassVar, Tuple
+from mecha import Mecha
 
 
 def load_tests(ctx: Context):
@@ -16,3 +17,9 @@ class TestFile(TextFile):
 def strip_tests(ctx: Context):
     for structure in ctx.data.structures.match("*:test_*"):
         del ctx.data.structures[structure]
+
+
+def skip_mecha_lint(ctx: Context):
+    """Disables the mecha linting step. Linting errors will be reported by the main project build"""
+    mc = ctx.inject(Mecha)
+    mc.steps.remove(mc.lint)
