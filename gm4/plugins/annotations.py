4ea32456213be22693430e0f050c90827ae19004
SpecialBuilder <specialbuilder32@gmail.com>
2024-02-21 23:41:22 -0500

    Add module root folder to logger

--- gm4/plugins/annotations.py
+++ gm4/plugins/annotations.py
@@ -1,15 +1,24 @@
 from beet import Context
 import logging
 import re
+from functools import partial
+from pathlib import Path
 
 def beet_default(ctx: Context):
     """Sets up a logging handler to repeat build log entries with the github action annotation format"""
-    beet_logger = logging.getLogger(None) # get root logger
+    root_logger = logging.getLogger(None) # get root logger
 
     handler = logging.StreamHandler()
     handler.setFormatter(AnnotationFormatter())
 
-    beet_logger.addHandler(handler)
+    root_logger.addHandler(handler)
+
+def add_module_dir_to_diagnostics(ctx: Context):
+    """Sets up a logging record factory that prepends the proper module folder to mecha diagnostics"""
+    local_filter = partial(add_mecha_subproject_dir, subproject_dir=ctx.directory.stem)
+    mc_logger = logging.getLogger("mecha")
+    mc_logger.addFilter(local_filter)
+
 
 LEVEL_CONVERSION = {
     logging.DEBUG: "debug",
@@ -33,3 +42,8 @@ class AnnotationFormatter(logging.Formatter):
 
         level = LEVEL_CONVERSION.get(record.levelno, logging.INFO)
         return f"::{level} file={filename},line={line},col={col}::{expl}"
+    
+def add_mecha_subproject_dir(record: logging.LogRecord, subproject_dir: str|Path = ""):
+    if d:=getattr(record, "annotate"):
+        record.annotate = f"{subproject_dir}\\{d}" # modify record in place
+    return True
