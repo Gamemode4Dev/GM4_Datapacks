fb2bb8a319627427e3ffc359c1a97c6d97a7c1b9
SpecialBuilder <specialbuilder32@gmail.com>
2024-03-05 19:33:49 -0500

    Separate RP and DP pack formats

--- gm4/plugins/write_mcmeta.py
+++ gm4/plugins/write_mcmeta.py
@@ -10,13 +10,20 @@ def beet_default(ctx: Context):
 	version = os.getenv("VERSION", "1.20")
 	manifest = ManifestCacheModel.parse_obj(ctx.cache["gm4_manifest"].json)
 	manifest_entry = {v.id:v for v in (manifest.modules|manifest.libraries).values()}.get(ctx.project_id, NoneAttribute())
-	
+
+	ctx.data.pack_format = 15 # manually set as beet's `latest` is not available for 1.20 at this time.
+	ctx.data.supported_formats = {
+		"min_inclusive": 15,
+		"max_inclusive": 26
+	}
+
+	ctx.assets.pack_format = 15 # manually set as beet's `latest` is not available for 1.20 at this time.
+	ctx.assets.supported_formats = {
+		"min_inclusive": 15,
+		"max_inclusive": 22
+	}
+
 	for pack in ctx.packs:
-		pack.pack_format = 15 # manually set as beet's `latest` is not available for 1.20 at this time.
-		pack.supported_formats = {
-			"min_inclusive": 15,
-			"max_inclusive": 26
-		}
 		pack.description = [
 			ctx.project_name,
 			"\n",
