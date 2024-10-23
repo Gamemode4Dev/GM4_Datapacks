ab3dbae52673e93a07edec02ae305edabe6bcebf
Misode <misoloo64@gmail.com>
2024-10-23 16:41:06 +0200

    Update to 1.21.3

--- gm4/plugins/write_mcmeta.py
+++ gm4/plugins/write_mcmeta.py
@@ -19,7 +19,7 @@ def beet_default(ctx: Context):
 			ctx.project_name,
 			"\n",
 			{
-				"text": f"Gamemode 4 for 1.21 - 1.21.2",
+				"text": f"Gamemode 4 for 1.21 - 1.21.3",
 				"color": "#4AA0C7"
 			}
 		]
