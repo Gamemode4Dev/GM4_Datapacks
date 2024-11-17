0bbbf2ac634144f9fc6247dbae141201df140f42
Misode <Misoloo64@gmail.com>
2024-11-17 21:08:47 +0100

    Implement custom crafting recipes as official JSON recipes (#1053)
    * Port custom crafting recipes to official JSON recipes when possible
    
    Leaving out: auto crafter, heart canister tier 2, relocators, scuba gear
    Slightly modifiers: ender hopper minecart, liquid minecart
    Discarding item data: boots of ostara, mountaineering
    
    * Add recipe unlock advancements
    
    * Update record crafting advancement
    
    * Optimize scuba gear advancement
    
    * Fix red_nether_bricks unlock was missing
    
    * Add group field to forming press recipes
    
    * Add standard crafting recipes except stairs and slabs
    
    * Generate standard crafting slabs and stairs recipes
    
    * Group stairs and slabs recipes together with vanilla recipes
    
    * Revert forming press changes
    
    * Update recipes to 1.21.3
    
    * Update standard crafting recipes
    
    * Remove attribute prefix from crampons recipe
    
    * Keep the old format for gm4_recipes
    
    * Backport recipes with beet
    
    * Handle when both attributes and recipe rewrites affect the same file
    
    * Catch recipe rewrite errors
    
    * Remove manually backported soul glass recipe

--- gm4/plugins/backwards.py
+++ gm4/plugins/backwards.py
@@ -1,5 +1,17 @@
-from beet import Context, TextFileBase
 import re
+import logging
+from typing import Any
+from beet import Context, TextFileBase, Recipe
+
+logger = logging.getLogger("gm4.backwards")
+
+# Generates overlays to support older versions
+def beet_default(ctx: Context):
+  yield
+
+  rewrite_attributes(ctx)
+  rewrite_recipes(ctx)
+
 
 ATTRIBUTES_RENAMES = {
   "minecraft:armor": "minecraft:generic.armor",
@@ -36,9 +48,8 @@ ATTRIBUTES_RENAMES = {
   "minecraft:water_movement_efficiency": "minecraft:generic.water_movement_efficiency",
 }
 
-def beet_default(ctx: Context):
-  yield
-
+# Removes the generic. and other prefixes from attribute IDs
+def rewrite_attributes(ctx: Context):
   for id, resource in ctx.data.all():
     if isinstance(resource, TextFileBase):
       resource.source_stop
@@ -51,3 +62,50 @@ def beet_default(ctx: Context):
         overlay = ctx.data.overlays["overlay_48"]
         overlay.supported_formats = { "min_inclusive": 48, "max_inclusive": 48 }
         overlay[id] = overlay_resource
+
+
+# Rewrites the recipe ingredients to the old {"item": "..."} format
+def rewrite_recipes(ctx: Context):
+  
+  def rewrite_ingredient(ingr: str | list[str]) -> Any:
+    if isinstance(ingr, list):
+      return [rewrite_ingredient(item) for item in ingr]
+    if ingr.startswith("#"):
+      return { "tag": ingr[1:] }
+    return { "item": ingr }
+  
+  def rewrite_recipe(id: str, resource: Recipe):
+    # If an overlay already exists for this recipe, us the contents of that
+    # TODO: generalize this for all rewrite functions and handle multiple overlays
+    for overlay in ctx.data.overlays.values():
+      if id in overlay.recipes:
+        resource = overlay.recipes[id]
+        break
+
+    overlay_resource = resource.copy()
+    data = overlay_resource.data
+
+    if "crafting_transmute" in data["type"]:
+      logger.warning(f"Cannot backport crafting_transmute recipe {id}")
+      return
+
+    if "base" in data:
+      data["base"] = rewrite_ingredient(data["base"])
+    if "addition" in data:
+      data["addition"] = rewrite_ingredient(data["addition"])
+    if "ingredient" in data:
+      data["ingredient"] = rewrite_ingredient(data["ingredient"])
+    if "ingredients" in data:
+      data["ingredients"] = [rewrite_ingredient(ingr) for ingr in data["ingredients"]]
+    if "key" in data:
+      data["key"] = {k: rewrite_ingredient(ingr) for k, ingr in data["key"].items()}
+
+    overlay = ctx.data.overlays["overlay_48"]
+    overlay.supported_formats = { "min_inclusive": 48, "max_inclusive": 48 }
+    overlay[id] = overlay_resource
+
+  for id, resource in ctx.data.recipes.items():
+    try:
+      rewrite_recipe(id, resource)
+    except BaseException as e:
+      logger.error(f"Failed to backport recipe {id}: {e}")
