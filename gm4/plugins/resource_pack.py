217b561ee5bae04c0ec081828bd7970e85222954
SpecialBuilder <specialbuilder32@gmail.com>
2025-04-16 21:21:19 -0400

    Add support for special-case vanilla tempates
    Covers player heads, which is used by metallurgy mould heads. NOTE: current implementation puts heads on an armor stand, bypassing the usual model rendering. If a custom block model is added to the moulds, migration to an item-display is required.

--- gm4/plugins/resource_pack.py
+++ gm4/plugins/resource_pack.py
@@ -1010,7 +1010,11 @@ class VanillaTemplate(TemplateOptions):
 
         ret_list: list[Model] = []
         for item, model_name in zip(config.item.entries(), model_names):
-            vanilla_model_path: str = self.vanilla_jar.assets.item_models[add_namespace(item, "minecraft")].data.get("model", {}).get("model", "") # type: ignore ; json access is string
+            model_compound = self.vanilla_jar.assets.item_models[add_namespace(item, "minecraft")].data.get("model", {})
+            if model_compound["type"] == "minecraft:special": # uses some special handling
+                vanilla_model_path: str = model_compound["base"] # covers player_head use case. Others may not be handled properly yet.
+            else:
+                vanilla_model_path: str = model_compound.get("model", "") # type: ignore ; json access is string
             m = models_container[model_name] = Model({      # type: ignore ; list is checked above to be all strings
                 "parent": vanilla_model_path
             })
