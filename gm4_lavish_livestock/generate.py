4650e6ca4d1ebbd2944eca047cbf15014b952b65
runcows <alexparbarnes@gmail.com>
2026-01-04 12:26:40 -0500

    Move list to beet.yaml

--- gm4_lavish_livestock/generate.py
+++ gm4_lavish_livestock/generate.py
@@ -5,21 +5,9 @@ from beet import Context, subproject
 logger = logging.getLogger(__name__)
 
 def beet_default(ctx: Context):
-    # List of supported entities
-    # This list is defined in 2 other places, translations.csv and the guidebook
-    entity_ids = [
-        "chicken",
-        "cow",
-        "hoglin",
-        "mooshroom",
-        "pig",
-        "rabbit",
-        "sheep"
-    ]
-    ctx.meta['animals'] = entity_ids
-    
+    # List of supported animals defined in beet.yaml as well as the guidebook page
     # for each supported entity, render a copy of the "templates" directory with the appropriate entity-id
-    for entity in entity_ids:
+    for entity in ctx.meta['animals']:
         subproject_config = {
             "data_pack": {
                 "load": [
