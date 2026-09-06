c8ea087ab285077ad9bc7ea68a8655bf4c543d4a
SpecialBuilder <specialbuilder32@gmail.com>
2026-09-06 13:33:47 -0400

    Send .beet_cache between build and publish jobs

--- gm4/plugins/publish.py
+++ gm4/plugins/publish.py
@@ -1,6 +1,14 @@
 from beet import Context, Project, ProjectBuilder, PackConfig
 from beet.toolchain.config import load_config
 
+def beet_default(ctx: Context):
+    pass
+
+def dev_test_1(ctx: Context):
+    print(f"beet cache restored as: {list(ctx.cache.keys())}")
+
+    print(f"gm4_manifest is :{ctx.cache["gm4_manifest"]}")
+
 def my_load_config(ctx: Context):
     """Loads relavent fields from each module's beet.yaml, without inheriting the pipeline and running plugins"""
 
