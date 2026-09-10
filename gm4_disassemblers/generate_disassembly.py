3a8d57ef113b22855e5c7d32b34c47a18d991411
runcows <alexparbarnes@gmail.com>
2026-09-10 13:22:57 -0400

    update version numbers used in plugins

--- gm4_disassemblers/generate_disassembly.py
+++ gm4_disassemblers/generate_disassembly.py
@@ -66,7 +66,7 @@ ITEMS = {
 def beet_default(ctx: Context):
   """Creates a loot table for dropping the 9 result items when disassembling an item."""
   vanilla = ctx.inject(Vanilla)
-  vanilla.minecraft_version = '26.2'
+  vanilla.minecraft_version = '26.3-rc-1'
   recipes = vanilla.data.recipes
 
   for item, durability in ITEMS.items():
